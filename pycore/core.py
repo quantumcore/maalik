e1405fdc1028f33cc5dcab8d0b005c95d30fc942
quantumcore <fastcorp11@gmail.com>
2020-09-06 09:39:05 +0500

    update logo

--- pycore/core.py
+++ pycore/core.py
@@ -977,7 +977,7 @@ def Console():
             print("Error : " + str(stre))
 
     clear_screen()
-    print(banner.format(ver = current_version()))
+    print(banner.format(makered = Style.BRIGHT + Fore.LIGHTRED_EX, makebright = Style.BRIGHT, ver = current_version()))
     _thread.start_new_thread(TCPServer, ()) 
     global silent
     while(True):
